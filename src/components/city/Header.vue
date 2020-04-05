5884faad5f30e686e79006b43618e3d004b225b1
yjf291488593 <291488593@qq.com>
2020-04-05 22:07:46 +0800

    city router

---
+++ src/components/city/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="header-left">
+        <span class="iconfont back-icon">&#xe7eb;</span>
+      </div>
+    </router-link>
+    <div class="header-title">城市选择</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CityHeader"
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/variables.styl'
+  .header
+    position relative
+    height $headerHeight
+    line-height $headerHeight
+    background $bgColor
+    text-align center
+    font-size .32rem
+    color #fff
+
+    .back-icon
+      position absolute
+      width .64rem
+      top 0
+      left 0
+      font-size .4rem
+      color #fff
+</style>
---
+++ src/views/City.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="city">
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+  import CityHeader from '../components/city/Header'
+
+  export default {
+    name: "City",
+    components:{
+      CityHeader
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
