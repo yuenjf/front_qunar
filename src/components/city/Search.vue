64727db191b9ec369989f227423fa6092951c4f8
yjf291488593 <291488593@qq.com>
2020-04-11 21:13:48 +0800

    city search finish

--- src/components/city/Search.vue
+++ src/components/city/Search.vue
@@ -1,12 +1,76 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content"
+         ref="wrapper"
+         v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom"
+            v-for="item of list"
+            :key="item.id"
+        >
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom"
+            v-show="hasNoData"
+        >
+          没有找到"{{keyword}}"相关城市
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  import BScroll from 'better-scroll'
+
   export default {
-    name: "CitySearch"
+    name: "CitySearch",
+    data() {
+      return {
+        keyword: '',
+        list: [],
+        timer: null
+      }
+    },
+    props: {
+      cities: Object
+    },
+    computed: {
+      hasNoData() {
+        return !this.list.length
+      }
+    },
+    watch: {
+      keyword() {
+        //  节流函数
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        if (!this.keyword) {
+          this.list = []
+          return
+        }
+        this.timer = setTimeout(() => {
+          //  先循环，在遍历出城市对象，与keyword匹配的就push进result
+          const result = []
+          for (let i in this.cities) {
+            this.cities[i].forEach(value => {
+              if (value.name.indexOf(this.keyword) > -1 ||
+                value.spell.indexOf(this.keyword) > -1) {
+                result.push(value)
+              }
+            })
+          }
+          this.list = result
+        }, 100)
+      }
+    },
+    mounted() {
+      this.scroll = new BScroll(this.$refs.wrapper)
+    }
   }
 </script>
 
@@ -26,4 +90,21 @@
       border-radius .06rem
       color #666
       padding 0 .1rem
+
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    left 0
+    background: #ccc;
+
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background #fff
+      color #666
+
 </style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="city">
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :hotCities="hotCities" :cities="cities" :letter="letter"></city-list>
     <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
